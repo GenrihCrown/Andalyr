91e48b3bbb2536639e65cf12cdd7b27f19e113e9
GenrihCrown <genrihcrown@gmail.com>
2021-10-30 12:22:28 +0300

    footer-update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,28 +1,31 @@
 import React from "react";
-import { Button } from "./Button"
-import './Footer.css'
+import { Button } from "./Button";
+import "./Footer.css";
 
 function Footer() {
   return (
     <div className="footer-container">
       <section className="footer-subscription">
         <p className="footer-subscription-text">
-            Узнавай первым о новостях из мира Андалира
+          Узнавай первым о новостях из мира Андалира
         </p>
         <div className="input-areas">
-            <form>
-                <input type="email" name="email" placeholder="Email" className="footer-input" />
-                <Button buttonStyle="btn--outline">Подписаться</Button>
-            </form>
+          <form>
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              className="footer-input"
+            />
+            <Button buttonStyle="btn--outline">Подписаться</Button>
+          </form>
         </div>
       </section>
-      <div className="footer-links">
-          <div className="footer-link-wrapper">
-              <div className="footer-link-items">
-                  <h2></h2>
-              </div>
-          </div>
-      </div>
+      <section className="social-media">
+        <div className="social-media-wrap">
+          <div className="social-media-logo"></div>
+        </div>
+      </section>
     </div>
   );
 }
