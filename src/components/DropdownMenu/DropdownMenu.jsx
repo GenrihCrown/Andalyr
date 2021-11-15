bb3ed9c1dec14dd56ab8365c3cca37e35411fa04
GenrihCrown <genrihcrown@gmail.com>
2021-11-15 22:43:47 +0300

    new-components

---
+++ src/components/DropdownMenu/DropdownMenu.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { ReactDOM } from "react-dom";
+
+class Dropdown extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            value: 'value', menuItems: [{id: 1, menuItem: 'Главы'}, {id: 2, menuItem: 'Статьи'}, {id: 3, menuItem: 'Карта'}, {id: 4, menuItem: 'Бестиарий'}]
+        }
+    
+    
+    
+    
+    
+    
+    }
+
+
+    
+    
+    render()
+}
